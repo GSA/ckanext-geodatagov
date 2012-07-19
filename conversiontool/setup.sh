d64e5ac8c244f87248c93ad11b178f4d33898267
Ross Jones <rossdjones@gmail.com>
2012-07-19 13:59:34 +0100

    Replace jars with a setup script

--- conversiontool/setup.sh
+++ conversiontool/setup.sh
@@ -4,4 +4,5 @@ mv saxon9pe.jar lib/
 rm saxon*.jar
 rm SaxonPE9-4-0-4J.zip
 
-wget http://jdbc.postgresql.org/download/postgresql-9.1-902.jdbc4.jar > lib/postgresql-9.1-902.jdbc4.jar
+wget http://jdbc.postgresql.org/download/postgresql-9.1-902.jdbc4.jar
+mv postgresql-9.1-902.jdbc4.jar lib/postgresql-9.1-902.jdbc4.jar
