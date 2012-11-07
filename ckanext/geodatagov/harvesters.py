de3f81b3b647198b5c5fc7b05014c3cb7c2794c1
amercader <amercadero@gmail.com>
2012-11-07 19:17:42 +0000

    New import stage logic (untested and with still lots to do)

--- ckanext/geodatagov/harvesters.py
+++ ckanext/geodatagov/harvesters.py
@@ -89,7 +89,198 @@ class GeoDataGovHarvester(SpatialHarvester):
     {"type":"Polygon","coordinates":[[[$minx, $miny],[$minx, $maxy], [$maxx, $maxy], [$maxx, $miny], [$minx, $miny]]]}
     ''')
 
+    def _validate_document(self, document_string, harvest_object, validator=None):
+        if not validator:
+            validator = self._get_validator()
+
+        xml = etree.fromstring(document_string)
+
+        valid, messages = validator.is_valid(xml)
+        if not valid:
+            log.error('Errors found for object with GUID %s:' % harvest_object.guid)
+            out = messages[0] + ':\n' + '\n'.join(messages[1:])
+            self._save_object_error(out, harvest_object,'Import')
+
+        return valid, messages
+
+
+    def _parse_iso_date(self, date_string):
+        try:
+            date = datetime.strptime(date_string, '%Y-%m-%d')
+        except ValueError:
+            try:
+                date = datetime.strptime(date_string,'%Y-%m-%dT%H:%M:%S')
+            except ValueError:
+                return None
+        return date
+
+
+    def _get_package_dict(self, iso_values, harvest_object):
+
+        #TODO: Big cleanup!
+
+        extras = {
+            'published_by': harvest_object.source.publisher_id or '',
+            'UKLP': 'True',
+            'harvest_object_id': harvest_object.id
+        }
+
+        # Just add some of the metadata as extras, not the whole lot
+        for name in [
+            # Essentials
+            'bbox-east-long',
+            'bbox-north-lat',
+            'bbox-south-lat',
+            'bbox-west-long',
+            'spatial-reference-system',
+            'guid',
+            # Usefuls
+            'dataset-reference-date',
+            'resource-type',
+            'metadata-language', # Language
+            'metadata-date', # Released
+            'coupled-resource',
+            'contact-email',
+            'frequency-of-update',
+            'spatial-data-service-type',
+        ]:
+            extras[name] = iso_values[name]
+
+        extras['guid'] = harvest_object.guid
+
+        extras['licence'] = iso_values.get('use-constraints', '')
+        if len(extras['licence']):
+            license_url_extracted = self._extract_first_license_url(extras['licence'])
+            if license_url_extracted:
+                extras['licence_url'] = license_url_extracted
+
+        extras['access_constraints'] = iso_values.get('limitations-on-public-access','')
+        if iso_values.has_key('temporal-extent-begin'):
+            extras['temporal_coverage-from'] = iso_values['temporal-extent-begin']
+        if iso_values.has_key('temporal-extent-end'):
+            extras['temporal_coverage-to'] = iso_values['temporal-extent-end']
+
+        # Save responsible organization roles
+        parties = {}
+        owners = []
+        publishers = []
+        for responsible_party in iso_values['responsible-organisation']:
+
+            if responsible_party['role'] == 'owner':
+                owners.append(responsible_party['organisation-name'])
+            elif responsible_party['role'] == 'publisher':
+                publishers.append(responsible_party['organisation-name'])
+
+            if responsible_party['organisation-name'] in parties:
+                if not responsible_party['role'] in parties[responsible_party['organisation-name']]:
+                    parties[responsible_party['organisation-name']].append(responsible_party['role'])
+            else:
+                parties[responsible_party['organisation-name']] = [responsible_party['role']]
+
+        parties_extra = []
+        for party_name in parties:
+            parties_extra.append('%s (%s)' % (party_name, ', '.join(parties[party_name])))
+        extras['responsible-party'] = '; '.join(parties_extra)
+
+        # Save provider in a separate extra:
+        # first organization to have a role of 'owner', and if there is none, first one with
+        # a role of 'publisher'
+        if len(owners):
+            extras['provider'] = owners[0]
+        elif len(publishers):
+            extras['provider'] = publishers[0]
+        else:
+            extras['provider'] = u''
+
+        # Construct a GeoJSON extent so ckanext-spatial can register the extent geometry
+        extent_string = self.extent_template.substitute(
+                minx = extras['bbox-east-long'],
+                miny = extras['bbox-south-lat'],
+                maxx = extras['bbox-west-long'],
+                maxy = extras['bbox-north-lat']
+                )
+
+        extras['spatial'] = extent_string.strip()
+
+        tags = []
+        for tag in iso_values['tags']:
+            tag = tag[:50] if len(tag) > 50 else tag
+            tags.append({'name':tag})
+
+        package_dict = {
+            'title': iso_values['title'],
+            'notes': iso_values['abstract'],
+            'tags': tags,
+            'resources':[]
+        }
+
+        if harvest_object.source.publisher_id:
+            package_dict['groups'] = [{'id': harvest_object.source.publisher_id}]
+
+        # TODO: package name
+        '''
+        if package is None or package.title != iso_values['title']:
+            name = self.gen_new_name(iso_values['title'])
+            if not name:
+                name = self.gen_new_name(str(gemini_guid))
+            if not name:
+                raise Exception('Could not generate a unique name from the title or the GUID. Please choose a more unique title.')
+            package_dict['name'] = name
+        else:
+            package_dict['name'] = package.name
+        '''
+
+        resource_locators = iso_values.get('resource-locator', [])
+
+        if len(resource_locators):
+            for resource_locator in resource_locators:
+                url = resource_locator.get('url','')
+                if url:
+                    resource_format = ''
+                    resource = {}
+                    if extras['resource-type'] == 'service':
+                        # Check if the service is a view service
+                        test_url = url.split('?')[0] if '?' in url else url
+                        if self._is_wms(test_url):
+                            resource['verified'] = True
+                            resource['verified_date'] = datetime.now().isoformat()
+                            resource_format = 'WMS'
+                    resource.update(
+                        {
+                            'url': url,
+                            'name': resource_locator.get('name',''),
+                            'description': resource_locator.get('description') if resource_locator.get('description') else 'Resource locator',
+                            'format': resource_format or None,
+                            'resource_locator_protocol': resource_locator.get('protocol',''),
+                            'resource_locator_function':resource_locator.get('function','')
+
+                        })
+                    package_dict['resources'].append(resource)
+
+            # Guess the best view service to use in WMS preview
+            verified_view_resources = [r for r in package_dict['resources'] if 'verified' in r and r['format'] == 'WMS']
+            if len(verified_view_resources):
+                verified_view_resources[0]['ckan_recommended_wms_preview'] = True
+            else:
+                view_resources = [r for r in package_dict['resources'] if r['format'] == 'WMS']
+                if len(view_resources):
+                    view_resources[0]['ckan_recommended_wms_preview'] = True
+
+        extras_as_dict = []
+        for key,value in extras.iteritems():
+            if isinstance(value,(basestring,Number)):
+                extras_as_dict.append({'key':key,'value':value})
+            else:
+                extras_as_dict.append({'key':key,'value':json.dumps(value)})
+
+        package_dict['extras'] = extras_as_dict
+
+        return package_dict
+
     def import_stage(self, harvest_object):
+
+        #TODO: exceptions
+
         log = logging.getLogger(__name__ + '.import')
         log.debug('Import stage for harvest object: %s', harvest_object.id)
 
@@ -101,6 +292,17 @@ class GeoDataGovHarvester(SpatialHarvester):
 
         # Save a reference
         self.obj = harvest_object
+
+        status = get_extra(harvest_object, 'status')
+
+        if status == 'delete':
+            # Delete package
+            context = {'model':model, 'session':Session, 'user':'harvest'} #TODO: user
+
+            get_action('package_delete')(context, {'id': harvest_object.package_id})
+
+            return True
+
         original_document = get_extra(harvest_object, 'original_document')
         if harvest_object.content is None and not original_document:
             self._save_object_error('Empty content for object %s' % self.obj.id,self.obj,'Import')
@@ -111,54 +313,147 @@ class GeoDataGovHarvester(SpatialHarvester):
         if original_format and original_format == 'fgdc':
 
             # Validate against FGDC schema
-            t = etree.fromstring(original_document)
-            validator = Validators(profiles=['fgdc'])
-            is_valid, errors = validator.is_valid(t)
+            is_valid, errors = self._validate_document(original_document, harvest_object,
+                                                       validator=Validators(profiles=['fgdc']))
             if not is_valid:
-                log.error('Errors found for object with GUID %s:' % self.obj.guid)
-                out = errors[0] + ':\n' + '\n'.join(errors[1:])
-                self._save_object_error(out,self.obj,'Import')
-
                 # TODO: Provide an option to continue anyway
                 return False
 
             transform_service = config.get('ckanext.geodatagov.fgdc2iso_service')
             if transform_service:
                 response = requests.post(transform_service, data=original_document.strip())
-
                 if response.status_code == 200:
                     self.obj.content = response.content
                     self.obj.save()
                 else:
-                    self._save_object_error(
-                            'The transformation service returned an error for object {0}: [{1}] {2}'.format(
-                                self.obj.id, response.status_code, response.content)
-                            ,self.obj,'Import')
+                    msg = 'The transformation service returned an error for object {0}'
+                    if response.status_code and response.content:
+                        msg += ': [{0}] {1}'.format(response.status_code, response.content)
+                    elif response.error:
+                        msg += ': {0}'.format(response.error)
+                        self._save_object_error(msg ,self.obj,'Import')
                     return False
 
+        else:
+            # Document is ISO, validate
+            is_valid, errors = self._validate_document(harvest_object.content, harvest_object)
+            if not is_valid:
+                # TODO: Provide an option to continue anyway
+                return False
 
-        #TODO: continue from here
-
+        # Parse ISO document
         try:
-            # Generate GUID if not present (i.e. it's a manual import)
-            if not self.obj.guid:
-                m = hashlib.md5()
-                m.update(self.obj.content.encode('utf8',errors='ignore'))
-                self.obj.guid = m.hexdigest()
-                self.obj.save()
-
-            self.import_gemini_object(harvest_object.content)
-
-            return True
+            iso_values = GeminiDocument(harvest_object.content).read_values()
         except Exception, e:
-            log.error('Exception during import: %s' % text_traceback())
-            if not str(e).strip():
-                self._save_object_error('Error importing document.', harvest_object, 'Import')
-            else:
-                self._save_object_error('Error importing document: %s' % str(e), harvest_object, 'Import')
+            self._save_object_error('Error parsing ISO document for object {0}'.format(harvest_object.id),
+                                    harvest_object,'Import')
+            return False
 
-            if debug_exception_mode:
-                raise
+        # Set old objects to current = False
+        model.Session.query(HarvestObject).\
+                    filter_by(guid=harvest_object.guid).\
+                    update({'current': False}, False)
+
+
+        # Update GUID with the one on the document
+        iso_guid = iso_values['guid']
+        if iso_guid and harvest_object.guid != iso_guid:
+            # First make sure there already aren't current objects
+            # with the same guid
+            existing_object = Session.query(HarvestObject.id) \
+                            .filter(HarvestObject.guid==iso_guid) \
+                            .filter(HarvestObject.current==True) \
+                            .first()
+            if existing_object:
+                self._save_object_error('Object {0} already has this guid {1}'.format(existing_object.id, iso_guid),
+                                    harvest_object,'Import')
+                return False
+
+            harvest_object.guid = iso_guid
+            harvest_object.save()
+
+        #TODO: do we still need this?
+        # Generate GUID if not present (i.e. it's a manual import)
+        if not self.obj.guid:
+            m = hashlib.md5()
+            m.update(self.obj.content.encode('utf8',errors='ignore'))
+            self.obj.guid = m.hexdigest()
+            self.obj.save()
+
+        # Get document modified date
+        metadata_modified_date = self._parse_iso_date(iso_values['metadata_date'])
+        if not metadata_modified_date:
+            self._save_object_error('Could not extract reference date for object {0} ({1})'
+                        .format(harvest_object.id, iso_values['metadata-date']))
+            return False
+        harvest_object.metadata_modified_date = metadata_modified_date
+        harvest_object.save()
+
+        # Build the package dict
+        package_dict = self._get_package_dict(iso_values)
+
+        # Create / update the package
+
+        context = {'model':model,
+                   'session':Session,
+                   'user':'harvest', # TODO: user
+                   'extras_as_string':True, # TODO: check if needed
+                   'api_version': '2',
+                   'return_id_only': True}
+
+        # The default package schema does not like Upper case tags
+        tag_schema = logic.schema.default_tags_schema()
+        tag_schema['name'] = [not_empty,unicode]
+
+        if status == 'create':
+            package_schema = logic.schema.default_create_package_schema()
+            package_schema['tags'] = tag_schema
+            context['schema'] = package_schema
+
+            # We need to explicitly provide a package ID, otherwise ckanext-spatial
+            # won't be be able to link the extent to the package.
+            package_dict['id'] = unicode(uuid.uuid4())
+            package_schema['id'] = [unicode]
+
+            try:
+                package_id = get_action('package_create')(context, package_dict)
+                log.info('Created new package %s with guid %s', package_id, harvest_object.guid)
+            except ValidationError,e:
+                raise Exception('Validation Error: %s' % str(e.error_summary))
+
+            # Save reference to the package on the object
+            harvest_object.package_id = package_id
+            harvest_object.save()
+
+        elif status == 'change':
+
+            # Get the last harvested object
+            previous_object = Session.query(HarvestObject) \
+                              .filter(HarvestObject.guid==harvest_object.guid) \
+                              .filter(HarvestObject.current==True) \
+                              .all()
+
+            # Check if the modified date is more recent
+            if harvest_object.metadata_modified_date <= previous_object.metadata_modified_date:
+                previous_object.delete()
+
+                log.info('Document with GUID %s unchanged, skipping...' % (harvest_object.guid))
+            else:
+                package_schema = logic.schema.default_update_package_schema()
+                package_schema['tags'] = tag_schema
+                context['schema'] = package_schema
+
+                package_dict['id'] = harvest_object.package_id
+
+                try:
+                    package_id = get_action('package_update')(context, package_dict)
+                    log.info('Updated package %s with guid %s', package_id, harvest_object.guid)
+                except ValidationError,e:
+                    raise Exception('Validation Error: %s' % str(e.error_summary))
+
+
+
+        return True
 
     def import_gemini_object(self, gemini_string):
         log = logging.getLogger(__name__ + '.import')
