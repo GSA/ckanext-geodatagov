e3e1d6b074608274f00752182cc87084bfa3c342
kindly <kindly@gmail.com>
2012-11-23 16:57:37 +0000

    make supervisor work

--- deployment/tmp/after_web.sh
+++ deployment/tmp/after_web.sh
@@ -3,5 +3,11 @@
 sed -i 's/^Listen.*/Listen 8080/g' /etc/httpd/conf/httpd.conf
 chkconfig --level 2345 nginx on
 chkconfig --level 2345 httpd on
+chkconfig --level 2345 rabbitmq-server on
+setsebool -P httpd_can_network_connect 1
+initctl reload-configuration
+
+initctl start supervisor
 service httpd restart
 service nginx restart
+service rabbitmq-server restart
