13168720d4358e1e4f6458eea6a9bebdf7056484
kindly <kindly@gmail.com>
2012-12-11 20:59:07 +0000

    add sebool for ctypes, geos, and dateutil dependancy

--- deployment/tmp/after_web.sh
+++ deployment/tmp/after_web.sh
@@ -4,6 +4,7 @@ sed -i 's/^Listen.*/Listen 8080/g' /etc/httpd/conf/httpd.conf
 chkconfig --level 2345 nginx on
 chkconfig --level 2345 httpd on
 setsebool -P httpd_can_network_connect 1
+setsebool -P httpd_tmp_exec on
 initctl reload-configuration
 rm -f /etc/nginx/conf.d/default.conf
 rm -f /etc/httpd/conf.d/welcome.conf
