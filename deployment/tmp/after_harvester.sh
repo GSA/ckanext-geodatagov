dfd525b17a134e3994a31c069d508496882a0492
joetsoi <joe.yeung.tsoi@gmail.com>
2013-02-12 13:09:50 +0000

    Add harvester cronjob
    updated epel rpm version, added cronie as dependency and
    a restart of the cron service in the rpm to make sure it's
    running

--- deployment/tmp/after_harvester.sh
+++ deployment/tmp/after_harvester.sh
@@ -2,8 +2,10 @@
 
 chkconfig --level 2345 rabbitmq-server on
 chkconfig --level 2345 tomcat6 on
+chkconfig --level 2345 crond on
 initctl reload-configuration
 
 initctl start supervisor
 service rabbitmq-server restart
 service tomcat6 restart
+service crond restart
