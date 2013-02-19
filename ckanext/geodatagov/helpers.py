3e303abe928034f335de4ab89bdf2cd937921e54
amercader <amercadero@gmail.com>
2013-02-19 18:55:15 +0000

    Add missing helpers file

---
+++ ckanext/geodatagov/helpers.py
@@ -0,0 +1,40 @@
+from ckan import plugins as p
+
+def get_harvest_object_formats(harvest_object_id):
+
+    obj = p.toolkit.get_action('harvest_object_show')({}, {'id': harvest_object_id})
+    
+    def get_extra(obj, key, default=None):
+        for k, v in obj['extras'].iteritems():
+            if k == key:
+                return v
+        return default
+
+    def format_title(format_name):
+        format_titles = {
+            'iso': 'ISO-10139',
+            'fgdc': 'FGDC',
+            'arcgis': 'ArcGIS Rest'
+        }
+        return format_titles[format_name] if format_name in format_titles else format_name
+
+    def format_type(format_name):
+        if format_name in ('iso', 'fgdc'):
+            format_type = 'xml'
+        elif format_name in ('arcgis'):
+            format_type = 'json'
+        else:
+            format_type = ''
+        return format_type
+
+
+    format_name = get_extra(obj, 'format', 'iso')
+    original_format_name = get_extra(obj, 'original_format')
+
+    return {
+            'object_format': format_title(format_name),
+            'object_format_type': format_type(format_name),
+            'original_format': format_title(original_format_name),
+            'original_format_type': format_type(original_format_name),
+            }
+
