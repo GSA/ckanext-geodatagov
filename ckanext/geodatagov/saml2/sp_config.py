20c32b2025695809776210338c54cce6d92a55fe
tobes <toby.junk@gmail.com>
2013-04-10 23:19:33 +0100

    Base saml2 config added

---
+++ ckanext/geodatagov/saml2/sp_config.py
@@ -0,0 +1,69 @@
+import os.path
+
+from saml2 import BINDING_HTTP_REDIRECT
+from saml2.saml import NAME_FORMAT_URI
+
+BASE= 'https://saml-test.datagov.ckan.org/'
+#BASE = 'http://localhost:5000/'
+CONFIG_PATH = os.path.dirname(__file__)
+
+USER_MAPPING = {
+    'email': 'mail',
+    'fullname': 'field_display_name',
+}
+
+CONFIG = {
+    'entityid' : 'urn:mace:umu.se:saml:ckan:sp',
+    'description': 'CKAN saml2 authorizor',
+    'service': {
+        'sp': {
+            'name' : 'CKAN SP',
+            'endpoints': {
+                'assertion_consumer_service': [BASE],
+                'single_logout_service' : [(BASE + 'slo',
+                                            BINDING_HTTP_REDIRECT)],
+            },
+            'required_attributes': [
+                'uid',
+                'name',
+                'mail',
+                'status',
+                'roles',
+                'field_display_name',
+                'realname',
+                # group stuff
+            ],
+            'optional_attributes': [],
+            'idp': ['urn:mace:umu.se:saml:ckan:idp'],
+        }
+    },
+    'debug': 0,
+    'key_file': CONFIG_PATH + '/pki/mykey.pem',
+    'cert_file': CONFIG_PATH + '/pki/mycert.pem',
+    'attribute_map_dir': CONFIG_PATH + '/attributemaps',
+    'metadata': {
+       'local': [CONFIG_PATH + '/idp.xml'],
+    },
+    # -- below used by make_metadata --
+    'organization': {
+        'name': 'Exempel AB',
+        'display_name': [('Exempel AB','se'),('Example Co.','en')],
+        'url':'http://www.example.com/ckan',
+    },
+    'contact_person': [{
+        'given_name':'John',
+        'sur_name': 'Smith',
+        'email_address': ['john.smith@example.com'],
+        'contact_type': 'technical',
+        },
+    ],
+    'name_form': NAME_FORMAT_URI,
+    'logger': {
+        'rotating': {
+            'filename': 'sp.log',
+            'maxBytes': 100000,
+            'backupCount': 5,
+            },
+        'loglevel': 'error',
+    }
+}
