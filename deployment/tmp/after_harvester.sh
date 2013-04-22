49fd491170cc412051d888c9ff57cc029987b40a
kindly <kindly@gmail.com>
2013-04-22 23:16:07 +0100

    release fixes, redis

--- deployment/tmp/after_harvester.sh
+++ deployment/tmp/after_harvester.sh
@@ -5,6 +5,11 @@ chkconfig --level 2345 tomcat6 on
 chkconfig --level 2345 crond on
 initctl reload-configuration
 
+if [ ! -f /usr/lib64/libxmlsec1-openssl.so ];
+then
+     ln -s /usr/lib64/libxmlsec1-openssl.so.1.2.16 /usr/lib64/libxmlsec1-openssl.so
+fi
+
 initctl start supervisor
 service rabbitmq-server restart
 service tomcat6 restart
--- deployment/tmp/after_web.sh
+++ deployment/tmp/after_web.sh
@@ -9,5 +9,10 @@ initctl reload-configuration
 rm -f /etc/nginx/conf.d/default.conf
 rm -f /etc/httpd/conf.d/welcome.conf
 
+if [ ! -f /usr/lib64/libxmlsec1-openssl.so ];
+then
+     ln -s /usr/lib64/libxmlsec1-openssl.so.1.2.16 /usr/lib64/libxmlsec1-openssl.so
+fi
+
 service httpd restart
 service nginx restart
