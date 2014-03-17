20a25005e4cbcef39bb2209946ed13c850b52a3f
ydave <ydave@reisystems.com>
2014-03-17 09:56:13 -0400

    browser url & fix

--- ckanext/geodatagov/fanstatic_library/scripts/sorting.js
+++ ckanext/geodatagov/fanstatic_library/scripts/sorting.js
@@ -208,6 +208,9 @@ jQuery( document ).ready(function() {
         browserURL = browserURL.replace('_' + id + '_sortCnt=desc', '');
         browserURL = browserURL.replace('?&', '?');
         browserURL = browserURL.replace('&&', '&');
+		
+		if(browserURL.slice(-1) == '&') 
+		   browserURL = browserURL.substring(0, browserURL.length - 1);
 
         $(this).parent().parent().find('p.module-footer a.read-more').attr('href', url);
 
@@ -298,6 +301,9 @@ jQuery( document ).ready(function() {
         browserURL = browserURL.replace('_' + id + '_sortAlpha=desc', '');
         browserURL = browserURL.replace('?&', '?');
         browserURL = browserURL.replace('&&', '&');
+		
+		if(browserURL.slice(-1) == '&') 
+		   browserURL = browserURL.substring(0, browserURL.length - 1);
 
         $(this).parent().parent().find('p.module-footer a.read-more').attr('href', url);
 
@@ -410,13 +416,17 @@ jQuery( document ).ready(function() {
 			var read_more_url = $("a[name='sm_" + id + "']").attr('href');
 			var flag = false;
 			if(read_more_url.indexOf("_" + id + "_limit=0") != -1)
-			   flag = true;
+			   flag = true;		   
 			
             $.each(listitems, function(idx, itm) {
                 if(cnt > 0) {
 				    if(flag == true) {
 					   var li_url = $(itm).find('a').attr('href');
 					   li_url = li_url.replace("_" + id + "_limit=0", "");
+					   
+					   if(li_url.slice(-1) == '&') 
+					      li_url = li_url.substring(0, li_url.length - 1);  
+						  
 					   $(itm).find('a').attr('href', li_url);
 					}
                     mylist1.append(itm);
