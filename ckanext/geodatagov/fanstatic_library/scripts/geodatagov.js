0207f1aed4964763ed81088f81bff2216c82dacd
kapil.bulchandani <kapil.bulchandani@reisystems.com>
2014-07-28 22:29:49 +0530

    adding the border and fixing the logic

--- ckanext/geodatagov/fanstatic_library/scripts/geodatagov.js
+++ ckanext/geodatagov/fanstatic_library/scripts/geodatagov.js
@@ -160,7 +160,7 @@ jQuery(window).load(function(){
 
         var comm_menus=[];
         var community = $.cookie('community_hash');
-        if ((typeof cookie !== 'undefined') && ('' !== cookie)) {
+        if ((typeof community !== 'undefined') && ('' !== community)) {
         $.each(json[community], function(i,comm_menu){
             if (comm_menu.link === '#'){
                 comm_menus.push('<li><a href="' +comm_menu.link + '">' +comm_menu.name + '</a><ul class="dropdown-menu">');
