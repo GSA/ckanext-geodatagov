310d4bc7be7cd3b89167c78171d40028fac5f6b3
fxia <fxia@reisystems.com>
2014-08-21 13:40:49 -0400

    stop httpd crond supervisord services before rpm

---
+++ deployment/tmp/before_harvester.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm -rf /usr/lib/ckan/bin /usr/lib/ckan/include /usr/lib/ckan/lib /usr/lib/ckan/lib64 /usr/lib/ckan/man
+
+service crond stop
+
+if [ $(ps -ef | grep "supervisord" | grep -v "grep" | wc -l) > 0 ];
+then
+    supervisorctl stop all
+fi
+
--- deployment/tmp/before_web.sh
+++ deployment/tmp/before_web.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 rm -rf /usr/lib/ckan/bin /usr/lib/ckan/include /usr/lib/ckan/lib /usr/lib/ckan/lib64 /usr/lib/ckan/man
+
+if [ $(ps -ef | grep "httpd" | grep -v "grep" | wc -l) > 0 ];
+then
+    service httpd stop
+fi
+
