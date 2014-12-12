8e91bd53449f1fa231bf457058cb71ee501987e8
yaditi <ydave@reisys.com>
2014-12-12 13:22:03 -0500

    Hide Contact Info, Publisher, Date and Access Level if

--- ckanext/geodatagov/fanstatic_library/scripts/geodatagov.js
+++ ckanext/geodatagov/fanstatic_library/scripts/geodatagov.js
@@ -154,6 +154,10 @@ jQuery(window).load(function(){
         linkRewriter("next.data.gov", "staging.data.gov");
 
     }
+
+    window.location.hash = "";
+
+   
 });
 
 if ($.browser.msie && $.browser.version == 10) {
@@ -189,4 +193,6 @@ $(document).ready(function () {
     $('.btn-preview').each(function() {
         test_to_preview($(this))
     });
+
+
 });
