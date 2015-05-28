28b08a76b2a8619f0093843732bc505a1f557ee9
fxia <fxia@reisystems.com>
2015-05-27 22:26:15 -0400

    remove not condition

--- deployment/tmp/after_web.sh
+++ deployment/tmp/after_web.sh
@@ -27,7 +27,7 @@ then
 fi
 
 chown apache:apache -R /usr/lib/ckan/src/ckanext-geodatagov/ckanext/geodatagov/dynamic_menu
-if [ ! -d /usr/lib/ckan/src/ckanext-datagovtheme/ckanext/datagovtheme/dynamic_menu ];
+if [ -d /usr/lib/ckan/src/ckanext-datagovtheme/ckanext/datagovtheme/dynamic_menu ];
 then
     chown apache:apache -R /usr/lib/ckan/src/ckanext-datagovtheme/ckanext/datagovtheme/dynamic_menu
 fi
