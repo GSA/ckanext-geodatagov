9f315d0459f0cb2bcba619d8c119705fc9901a28
fxia <fxia@reisystems.com>
2016-12-13 15:07:22 -0500

    stuck harvest job force finish bug

--- ckanext/geodatagov/commands.py
+++ ckanext/geodatagov/commands.py
@@ -649,8 +649,8 @@ select DOCUUID, TITLE, OWNER, APPROVALSTATUS, HOST_URL, Protocol, PROTOCOL_TYPE,
             email_log('harvest-job-cleanup', msg)
             return
 
-        harvest_pairs_1 = []
-        harvest_pairs_2 = []
+        harvest_pairs_1 = [] # stuck jobs with harvest objects
+        harvest_pairs_2 = [] # stuck jobs without harvest objects
         # find those stuck jobs with harvest source
         create_time_limit = '12 hours'
         fetch_time_limit = '6 hours'
@@ -748,12 +748,12 @@ select DOCUUID, TITLE, OWNER, APPROVALSTATUS, HOST_URL, Protocol, PROTOCOL_TYPE,
                 id = :harvest_job_id
         '''
 
-        for item in harvest_pairs_2:
+        for item in harvest_pairs_1 + harvest_pairs_2:
             model.Session.execute(sql, {'harvest_job_id': item['harvest_job_id']})
             model.Session.commit()
-
-        for item in harvest_pairs_1 + harvest_pairs_2:
-             msg += str(datetime.datetime.now()) + ' Harvest source %s was forced to Finish.\n' % item[
+            if item in harvest_pairs_1:
+                self.harvest_object_relink(item['harvest_source_id'])
+            msg += str(datetime.datetime.now()) + ' Harvest source %s was forced to Finish.\n' % item[
                  'harvest_source_id']
 
         if not (harvest_pairs_1 + harvest_pairs_2):
