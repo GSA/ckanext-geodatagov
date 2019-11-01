753324b73f23d03c70b3e771da568ef239d91005
Andres Vazquez <andres@data99.com.ar>
2019-11-01 11:25:19 -0300

    install postgis

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -6,7 +6,7 @@ echo "This is travis-build.bash..."
 echo "-----------------------------------------------------------------"
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
-sudo apt-get install solr-jetty libcommons-fileupload-java libpq-dev postgresql postgresql-contrib python-lxml
+sudo apt-get install solr-jetty libcommons-fileupload-java libpq-dev postgresql postgresql-contrib python-lxml postgresql-9.3-postgis-2.1
 
 
 pip install setuptools -U
