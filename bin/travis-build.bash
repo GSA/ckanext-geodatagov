b5fb8eae241a3a229c330e9e5481f410d7089248
Andres Vazquez <andres@data99.com.ar>
2019-12-16 15:49:55 -0300

    fix postgresql version for CKAN 2.3

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -5,18 +5,17 @@ echo "This is travis-build.bash..."
 echo "-----------------------------------------------------------------"
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
-sudo apt-get install solr-jetty libcommons-fileupload-java libpq-dev postgresql postgresql-contrib python-lxml postgresql-9.3-postgis-2.1
+sudo apt-get install solr-jetty libcommons-fileupload-java libpq-dev postgresql postgresql-contrib python-lxml
 
+if [ $CKANVERSION == '2.8' ]
+then
+	sudo apt-get install postgresql-9.3-postgis-2.1
+elif [ $CKANVERSION == '2.3' ]
+then
+	sudo apt-get install postgresql-9.1-postgis
+fi
 
 pip install setuptools -U
-
-# if [ $CKANVERSION == '2.8' ]
-# then
-# 	sudo apt-get install postgresql-9.6
-# elif [ $CKANVERSION == '2.3' ]
-# then
-# 	sudo apt-get install postgresql-9.1
-# fi
 
 echo "-----------------------------------------------------------------"
 echo "Installing CKAN and its Python dependencies..."
