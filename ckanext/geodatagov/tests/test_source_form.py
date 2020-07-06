8625c46d972fb5b38acd935583e1c9d388e58b03
Andres Vazquez <andres@data99.com.ar>
2020-07-06 14:12:08 -0300

    Add test for database and port fields

--- ckanext/geodatagov/tests/test_source_form.py
+++ ckanext/geodatagov/tests/test_source_form.py
@@ -17,19 +17,18 @@ class TestHarvestSourceForm(helpers.FunctionalTestBase):
         sysadmin = factories.Sysadmin()
         cls.extra_environ = {'REMOTE_USER': sysadmin['name'].encode('ascii')}
 
-
-    def test_create_harvest_source_form(self):
+    def test_create_waf_collection_harvest_source_form(self):
 
         self.app = self._get_test_app()
         
         # Create
         res = self.app.get('/harvest/new', extra_environ=self.extra_environ)
         
-        harvest_source_name = u'test-harvest-source'
+        harvest_source_name = u'test-waf-collection-harvest-source'
         fv = res.forms['source-new']
         fv['url'] = u'https://meta.geo.census.gov/data/existing/decennial/GEO/GPMB/TIGERline/TIGER2018/concity/'
         fv['source_type'] = u'waf-collection'
-        fv['title'] = u'Test harvest source'
+        fv['title'] = u'Test WAF colelction harvest source'
         fv['name'] = harvest_source_name
         fv['collection_metadata_url'] = 'https://meta.geo.census.gov/data/existing/decennial/GEO/GPMB/TIGERline/TIGER2018/SeriesInfo/SeriesCollection_tl_2018_concity.shp.iso.xml'
 
@@ -46,3 +45,34 @@ class TestHarvestSourceForm(helpers.FunctionalTestBase):
         assert_equal(fv['collection_metadata_url'].value, 'https://meta.geo.census.gov/data/existing/decennial/GEO/GPMB/TIGERline/TIGER2018/SeriesInfo/SeriesCollection_tl_2018_concity.shp.iso.xml')
         assert_equal(fv['url'].value, 'https://meta.geo.census.gov/data/existing/decennial/GEO/GPMB/TIGERline/TIGER2018/concity/')
         assert_equal(fv['source_type'].value, 'waf-collection')
+
+    def test_create_z3950_harvest_source_form(self):
+
+        self.app = self._get_test_app()
+        
+        # Create
+        res = self.app.get('/harvest/new', extra_environ=self.extra_environ)
+        
+        harvest_source_name = u'test-harvest-source'
+        fv = res.forms['source-new']
+        fv['url'] = u'https://test.z3950.com/'
+        fv['source_type'] = u'z3950'
+        fv['title'] = u'Test Z3950 harvest source'
+        fv['name'] = harvest_source_name
+        fv['database'] = 'test-database'
+        fv['port'] = '9999'
+
+        # Save
+        res = fv.submit('save', extra_environ=self.extra_environ)
+        assert_not_in('Error', res)
+        assert_not_in('Missing value', res)
+
+        # Go to the edit form
+        res_redirect = self.app.get('/harvest/edit/{}'.format(harvest_source_name), extra_environ=self.extra_environ)
+        
+        # ensure we have the expected values
+        fv = res_redirect.forms['source-new']
+        assert_equal(fv['url'].value, 'https://test.z3950.com/')
+        assert_equal(fv['source_type'].value, 'z3950')
+        assert_equal(fv['database'].value, 'test-database')
+        assert_equal(fv['port'].value, '9999')
