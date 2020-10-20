327371bd5c963f6542e3bb6a0a14a094626851cd
Andres Vazquez <andres@data99.com.ar>
2020-10-20 09:43:24 -0300

    Fix merged URL

--- bin/travis-build-2.8.bash
+++ bin/travis-build-2.8.bash
@@ -3,8 +3,8 @@ set -e
 
 echo "Updating script ..."
 
-wget https://raw.githubusercontent.com/GSA/catalog.data.gov/canonical_script/tools/ci-scripts/circleci-build-catalog-next.bash
-wget https://raw.githubusercontent.com/GSA/catalog.data.gov/canonical_script/tools/ci-scripts/test-catalog-next.ini
+wget https://raw.githubusercontent.com/GSA/catalog.data.gov/master/tools/ci-scripts/circleci-build-catalog-next.bash
+wget https://raw.githubusercontent.com/GSA/catalog.data.gov/master/tools/ci-scripts/test-catalog-next.ini
 
 sudo chmod +x circleci-build-catalog-next.bash
 source circleci-build-catalog-next.bash
