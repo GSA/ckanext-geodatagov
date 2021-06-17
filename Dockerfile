41d970c1711cfa1bb4eac7ceea71a8a47922c377
Nicholas Kumia <nicholas.kumia@reisystems.com>
2021-06-17 09:05:25 -0400

    fixed initial set of pip requirements.. copied the missing requirements from catalog.data.gov :$

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 ARG CKAN_VERSION=2.8
 FROM openknowledge/ckan-dev:${CKAN_VERSION}
 
+RUN apk add geos-dev
+
 COPY . /app
 WORKDIR /app
 
--- PyZ3950_parsetab.py
+++ PyZ3950_parsetab.py
@@ -5,7 +5,7 @@ _tabversion = '3.2'
 
 _lr_method = 'LALR'
 
-_lr_signature = '\xad\x9f;S\xfdL\x93\xee\xcb\xb3\x95\xd7\xd43\xd7i'
+_lr_signature = '\x1b@\xe2\t\xda\xb8\x91\xe6[\x96\xc7{4=\xa1\xd4K\xee\xf3\xb1'
     
 _lr_action_items = {'QUOTEDVALUE':([0,12,14,18,26,],[1,1,1,1,1,]),'LOGOP':([1,3,4,5,6,13,19,20,22,24,25,27,],[-12,-5,-14,-8,14,-13,14,-4,-7,-9,-6,14,]),'SET':([0,12,14,26,],[10,10,10,10,]),'WORD':([0,1,4,5,12,13,14,15,16,18,24,26,],[4,-12,-14,13,4,-13,4,21,22,4,13,4,]),'RELOP':([9,10,11,23,],[-10,16,18,-11,]),'SLASH':([21,],[26,]),'ATTRSET':([0,],[8,]),'QUAL':([0,12,14,17,26,],[9,9,9,23,9,]),'COMMA':([9,11,23,],[-10,17,-11,]),'LPAREN':([0,8,12,14,26,],[12,15,12,12,12,]),'RPAREN':([1,3,4,5,13,19,20,22,24,25,27,],[-12,-5,-14,-8,-13,25,-4,-7,-9,-6,28,]),'$end':([1,2,3,4,5,6,7,13,20,22,24,25,28,],[-12,-1,-5,-14,-8,-2,0,-13,-4,-7,-9,-6,-3,]),}
 
@@ -26,18 +26,18 @@ for _k, _v in _lr_goto_items.items():
 del _lr_goto_items
 _lr_productions = [
   ("S' -> top","S'",1,None,None,None),
-  ('top -> cclfind_or_attrset','top',1,'p_top','/usr/lib/ckan/src/py3250/PyZ3950/ccl.py',170),
-  ('cclfind_or_attrset -> cclfind','cclfind_or_attrset',1,'p_cclfind_or_attrset_1','/usr/lib/ckan/src/py3250/PyZ3950/ccl.py',174),
-  ('cclfind_or_attrset -> ATTRSET LPAREN WORD SLASH cclfind RPAREN','cclfind_or_attrset',6,'p_cclfind_or_attrset_2','/usr/lib/ckan/src/py3250/PyZ3950/ccl.py',178),
-  ('cclfind -> cclfind LOGOP elements','cclfind',3,'p_ccl_find_1','/usr/lib/ckan/src/py3250/PyZ3950/ccl.py',182),
-  ('cclfind -> elements','cclfind',1,'p_ccl_find_2','/usr/lib/ckan/src/py3250/PyZ3950/ccl.py',186),
-  ('elements -> LPAREN cclfind RPAREN','elements',3,'p_elements_1','/usr/lib/ckan/src/py3250/PyZ3950/ccl.py',190),
-  ('elements -> SET RELOP WORD','elements',3,'p_elements_2','/usr/lib/ckan/src/py3250/PyZ3950/ccl.py',212),
-  ('elements -> val','elements',1,'p_elements_3','/usr/lib/ckan/src/py3250/PyZ3950/ccl.py',218),
-  ('elements -> quallist RELOP val','elements',3,'p_elements_4','/usr/lib/ckan/src/py3250/PyZ3950/ccl.py',222),
-  ('quallist -> QUAL','quallist',1,'p_quallist_1','/usr/lib/ckan/src/py3250/PyZ3950/ccl.py',229),
-  ('quallist -> quallist COMMA QUAL','quallist',3,'p_quallist_2','/usr/lib/ckan/src/py3250/PyZ3950/ccl.py',233),
-  ('val -> QUOTEDVALUE','val',1,'p_val_1','/usr/lib/ckan/src/py3250/PyZ3950/ccl.py',237),
-  ('val -> val WORD','val',2,'p_val_2','/usr/lib/ckan/src/py3250/PyZ3950/ccl.py',241),
-  ('val -> WORD','val',1,'p_val_3','/usr/lib/ckan/src/py3250/PyZ3950/ccl.py',245),
+  ('top -> cclfind_or_attrset','top',1,'p_top','/srv/app/src/py3250/PyZ3950/ccl.py',170),
+  ('cclfind_or_attrset -> cclfind','cclfind_or_attrset',1,'p_cclfind_or_attrset_1','/srv/app/src/py3250/PyZ3950/ccl.py',174),
+  ('cclfind_or_attrset -> ATTRSET LPAREN WORD SLASH cclfind RPAREN','cclfind_or_attrset',6,'p_cclfind_or_attrset_2','/srv/app/src/py3250/PyZ3950/ccl.py',178),
+  ('cclfind -> cclfind LOGOP elements','cclfind',3,'p_ccl_find_1','/srv/app/src/py3250/PyZ3950/ccl.py',182),
+  ('cclfind -> elements','cclfind',1,'p_ccl_find_2','/srv/app/src/py3250/PyZ3950/ccl.py',186),
+  ('elements -> LPAREN cclfind RPAREN','elements',3,'p_elements_1','/srv/app/src/py3250/PyZ3950/ccl.py',190),
+  ('elements -> SET RELOP WORD','elements',3,'p_elements_2','/srv/app/src/py3250/PyZ3950/ccl.py',212),
+  ('elements -> val','elements',1,'p_elements_3','/srv/app/src/py3250/PyZ3950/ccl.py',218),
+  ('elements -> quallist RELOP val','elements',3,'p_elements_4','/srv/app/src/py3250/PyZ3950/ccl.py',222),
+  ('quallist -> QUAL','quallist',1,'p_quallist_1','/srv/app/src/py3250/PyZ3950/ccl.py',229),
+  ('quallist -> quallist COMMA QUAL','quallist',3,'p_quallist_2','/srv/app/src/py3250/PyZ3950/ccl.py',233),
+  ('val -> QUOTEDVALUE','val',1,'p_val_1','/srv/app/src/py3250/PyZ3950/ccl.py',237),
+  ('val -> val WORD','val',2,'p_val_2','/srv/app/src/py3250/PyZ3950/ccl.py',241),
+  ('val -> WORD','val',1,'p_val_3','/srv/app/src/py3250/PyZ3950/ccl.py',245),
 ]
