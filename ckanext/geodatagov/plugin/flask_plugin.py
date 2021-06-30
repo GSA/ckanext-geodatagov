03fb1a73c9c75c37de51acc07ac340e23cc77b7b
Nicholas Kumia <nicholas.kumia@reisystems.com>
2021-06-30 11:19:49 -0400

    (optional) Create Mixin plugin implementations for Pylons / Flask (part 2?).. updated MixinPlugin based on https://github.com/GSA/ckanext-datagovtheme/blob/e523db00be877f7b20262291ca5a627c6eb1ed11/ckanext/datagovtheme/plugin/flask_plugin.py

--- ckanext/geodatagov/plugin/flask_plugin.py
+++ ckanext/geodatagov/plugin/flask_plugin.py
@@ -4,4 +4,14 @@ Mixin for Flask-specific functionality. This aides the migration between Pylons 
 
 
 class MixinPlugin(object):
-    pass
+
+    # IConfigurer
+    def update_config(self, config):
+        # TODO remove template/templates_2_8 and move templates/templates_new
+        # to templates once we're off of CKAN 2.8.
+        #
+        # Using a separate dir for templates avoids having to maintain
+        # backwards compatibility using a sprinkling of conditionals. We don't
+        # anticipate adding new features to the existing 2.8 templates.
+        p.toolkit.add_template_directory(config, '../templates/templates_new')
+        p.toolkit.add_resource('../fanstatic_library', 'geodatagov')
