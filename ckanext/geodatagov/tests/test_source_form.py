c9a9ef81670feb03f188059b4282f65fd611b378
Nicholas Kumia <nicholas.kumia@gsa.gov>
2021-07-19 18:14:52 -0400

    updated test for source form.. the CKAN test suite no longer supports forms in web pages; so custom parsing needs to be done to extract form options and data, https://docs.python.org/3/library/html.parser.html

--- ckanext/geodatagov/tests/test_source_form.py
+++ ckanext/geodatagov/tests/test_source_form.py
@@ -1,5 +1,7 @@
+from html.parser import HTMLParser
 import logging
 import six
+from webtest import TestApp
 
 import ckanext.harvest.model as harvest_model
 
@@ -7,6 +9,43 @@ from ckan.tests import helpers, factories
 
 log = logging.getLogger(__name__)
 
+class SourceFormParser(HTMLParser):
+    results = {
+        'url': None,
+        'source_type': None,
+        'title': None,
+        'name': None,
+        'collection_metadata_url': None,
+        'validator_profiles': None
+    }
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'input':
+            checked = False
+            attr_dict = {}
+            for attr in attrs:
+                attr_dict[attr[0]] = attr[1]
+            if attr_dict['name'] == 'url':
+                self.results['url'] = attr_dict['value']
+            if attr_dict['name'] == 'title':
+                self.results['title'] = attr_dict['value']
+            if attr_dict['name'] == 'name':
+                self.results['name'] = attr_dict['value']
+            if attr_dict['name'] == 'collection_metadata_url':
+                self.results['collection_metadata_url'] = attr_dict['value']
+            if 'checked' in attr_dict:
+                if attr_dict['name'] == 'source_type':
+                    if attr_dict['value'] != '':
+                        self.results['source_type'] = attr_dict['value']
+                if attr_dict['name'] == 'validator_profiles':
+                    if attr_dict['value'] != '':
+                        self.results['validator_profiles'] = attr_dict['value']
+
+    def handle_endtag(self, tag):
+        pass
+
+    def handle_data(self, data):
+        pass
 
 class TestHarvestSourceForm(helpers.FunctionalTestBase):
 
@@ -31,31 +70,56 @@ class TestHarvestSourceForm(helpers.FunctionalTestBase):
         self.app = self._get_test_app()
 
         # Create
-        res = self.app.get('/harvest/new', extra_environ=self.extra_environ)
-
         harvest_source_name = 'test-waf-collection-harvest-source'
-        fv = res.forms['source-new']
-        fv['url'] = WAF_HARVEST_SOURCE_URL
-        fv['source_type'] = 'waf-collection'
-        fv['title'] = 'Test WAF colelction harvest source'
-        fv['name'] = harvest_source_name
-        fv['collection_metadata_url'] = COLLECTION_METADATA_URL
-        fv['validator_profiles'] = 'iso19139ngdc'
 
-        # Save
-        res = fv.submit('save', extra_environ=self.extra_environ)
-        assert 'Error' not in res
-        assert 'Missing value' not in res
+        if six.PY2:
+            res = self.app.get('/harvest/new', extra_environ=self.extra_environ)
+            fv = res.forms['source-new']
+            fv['url'] = WAF_HARVEST_SOURCE_URL
+            fv['source_type'] = 'waf-collection'
+            fv['title'] = 'Test WAF colelction harvest source'
+            fv['name'] = harvest_source_name
+            fv['collection_metadata_url'] = COLLECTION_METADATA_URL
+            fv['validator_profiles'] = 'iso19139ngdc'
 
-        # Go to the edit form
-        res_redirect = self.app.get('/harvest/edit/{}'.format(harvest_source_name), extra_environ=self.extra_environ)
+            # Save
+            res = fv.submit('save', extra_environ=self.extra_environ)
+            assert 'Error' not in res
+            assert 'Missing value' not in res
 
-        # ensure we have the expected values
-        fv = res_redirect.forms['source-new']
-        assert fv['collection_metadata_url'].value == COLLECTION_METADATA_URL
-        assert fv['url'].value == WAF_HARVEST_SOURCE_URL
-        assert fv['source_type'].value == 'waf-collection'
-        assert fv['validator_profiles'].value == 'iso19139ngdc'
+            # Go to the edit form
+            res_redirect = self.app.get('/harvest/edit/{}'.format(harvest_source_name), extra_environ=self.extra_environ)
+
+            # ensure we have the expected values
+            fv = res_redirect.forms['source-new']
+            assert fv['collection_metadata_url'].value == COLLECTION_METADATA_URL
+            assert fv['url'].value == WAF_HARVEST_SOURCE_URL
+            assert fv['source_type'].value == 'waf-collection'
+            assert fv['validator_profiles'].value == 'iso19139ngdc'
+        else:
+            parser = SourceFormParser()
+            form_data = {
+                'url': WAF_HARVEST_SOURCE_URL,
+                'source_type': 'waf-collection',
+                'title': 'Test WAF colelction harvest source',
+                'name': harvest_source_name,
+                'collection_metadata_url': COLLECTION_METADATA_URL,
+                'validator_profiles': 'iso19139ngdc'
+            }
+            res = self.app.post('/harvest/new', params=form_data, extra_environ=self.extra_environ)
+
+            assert 'Error' not in res.body
+            assert 'Missing value' not in res.body
+
+            res_redirect = self.app.get('/harvest/edit/{}'.format(harvest_source_name), extra_environ=self.extra_environ)
+            parser.feed(res_redirect.body)
+            results = parser.results
+
+            assert results['collection_metadata_url'] == COLLECTION_METADATA_URL
+            assert results['url'] == WAF_HARVEST_SOURCE_URL
+            assert results['source_type'] == 'waf-collection'
+            assert results['validator_profiles'] == 'iso19139ngdc'
+            #print(res_redirect)
 
     def test_create_z3950_harvest_source_form(self):
 
