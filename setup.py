dbc63ff2a2c6de7ad1adea2f3b6eaeebdb5ed45e
Nicholas Kumia <nicholas.kumia@reisystems.com>
2022-09-14 10:25:15 -0400

    new cleanup: no more future dependency

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ckanext-geodatagov',
-    version='0.1.4',
+    version='0.1.5',
     description="",
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -31,7 +31,6 @@ setup(
         'ckanext-datajson',
         'boto3',
         'ply>=3.4',
-        'future>=0.18.2'
     ],
     setup_requires=['wheel'],
     entry_points="""
