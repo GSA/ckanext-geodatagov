00380030d32eccaf750b23eb6323eee83086920a
robert-bryson <robert.bryson@gsa.gov>
2022-12-15 11:19:08 -0800

    split tests, add check for content-type

--- ckanext/geodatagov/tests/test_s3test.py
+++ ckanext/geodatagov/tests/test_s3test.py
@@ -33,17 +33,31 @@ class TestS3TestCommand(object):
 
         return raw_cli_output
 
-    def test_s3_upload(self, html_cli_result, txt_cli_result):
+    def test_s3_upload_txt(self, txt_cli_result):
         """upload test.txt to s3 and make sure there's no errors"""
         # check successful cli run
-        assert html_cli_result.exit_code == 0
         assert txt_cli_result.exit_code == 0
 
         endpoint_url = config.get("ckanext.s3sitemap.endpoint_url")
         bucket = config.get("ckanext.s3sitemap.aws_bucket_name")
 
-        uploaded_html_file = requests.get(f"{endpoint_url}/{bucket}/test.html")
-        assert html_cli_result.output.strip("\n") == uploaded_html_file.content.decode("utf8")
+        s3_response = requests.get(f"{endpoint_url}/{bucket}/test.txt")
+        assert txt_cli_result.output.strip("\n") == s3_response.content.decode("utf8")
 
-        uploaded_txt_file = requests.get(f"{endpoint_url}/{bucket}/test.txt")
-        assert txt_cli_result.output.strip("\n") == uploaded_txt_file.content.decode("utf8")
+        # check content-type
+        assert 'text/plain' == s3_response.headers['content-type']
+
+    def test_s3_upload_html(self, html_cli_result):
+        """upload test.html to s3 and make sure there's no errors"""
+        # check successful cli run
+        assert html_cli_result.exit_code == 0
+
+        endpoint_url = config.get("ckanext.s3sitemap.endpoint_url")
+        bucket = config.get("ckanext.s3sitemap.aws_bucket_name")
+
+        # chcek content
+        s3_response = requests.get(f"{endpoint_url}/{bucket}/test.html")
+        assert html_cli_result.output.strip("\n") == s3_response.content.decode("utf8")
+
+        # check content-type
+        assert 'application/html' == s3_response.headers['content-type']
