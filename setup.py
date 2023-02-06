a379b42c567c275a65276edebe5acd1d19e66a40
Jin-Sun-tts <jin.sun@gsa.gov>
2023-02-06 18:40:21 -0500

    bump pypi version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="ckanext-geodatagov",
-    version="0.1.24",
+    version="0.1.25",
     description="",
     long_description=long_description,
     long_description_content_type='text/markdown',
